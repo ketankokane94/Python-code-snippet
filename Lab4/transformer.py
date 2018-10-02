3d8590d8dab55b15ac6652fef1d1963cd5765d55
ketankokane94 <ketankokane94@gmail.com>
2018-10-02 13:19:32 -0400

    implemented the decryption

--- Lab4/transformer.py
+++ Lab4/transformer.py
@@ -1,32 +1,20 @@
 __author__ = 'KK'
 __author__ = 'SB'
 
-# for the polygon of side 8 the program takes around ~15 secs to complete
+
 """
-CSCI-603 : Polygons Lab-3 (week 4)
+CSCI-603 : transformer Lab-4 (week 5)
 Author : Ketan Kokane
 Author : Siddharth Bapat
-This program draws the name of the authors on top right corner,
-reads the parameters sides and fill no fill from command line and
-draws the polygons with the given sides, with fill no fill option also,
-creates pattern by drawing the polygons of reducing sides and length until
-no more polygons can be drawn.
-The basic pattern used is to draw smaller polygon on one third length of the
-main polygon at an angle of 20 degree.
+
 """
 
 import sys
 
-"""
-Python provides theordfunction to convert a single character to an integer
-ASCIIvalue, and thechrfunction to convert from an ASCII integer to a character.
-Withthat in mind, show how to implement shifting of a single uppercase charactercbya
- positive  amountk,  keeping  in  mind  that  if  this  takes  you  past  the  end  of
-  thealphabet, it should wrap around.
-"""
 
+OPERATIONS_SEPERATOR = ';'
 
-def shift(message, index=0, exponent=1):
+def shift(message, index = 0, exponent = 1):
     '''
     this program takes a string and shifts the character at the index
     by exponent units to right or left depending on + -
@@ -103,7 +91,7 @@ def swap(message, left, right):
         return message
 
 
-def get_groups(message, group_size):
+def split_into_groups(message, group_size):
     """
     this function gets a string and a positive group size and returns the
     chunks of the same size
@@ -138,54 +126,111 @@ def group_swap(message,left,right,group_size):
         # is even length and can be reversed
         if len(message) % 2 == 1:
             message = message + "$"
-        message = get_groups(message,group_size)
+        message = split_into_groups(message, group_size)
         return swap(message, left, right)
 
 
-
-def generate_call(message,operator):
+def make_call_rotate(message, operator, encrypt_message):
     """
-    this is a helper function which splits the operator to the required
-    format to make call to the shift function and returns its ouput
+    this function is responsible to use the rotate transformation for encrypt and decrypt
+    by preprocessing it a little
+
     :param message:
     :param operator:
+    :param encrypt_message:
     :return:
     """
-    # the operator is always of the form S8, 9
+    if len(operator) == 1:
+        if encrypt_message:
+            return rotate(message)
+        else:
+            return rotate(message, -1)
+    characters_to_rotate = int(operator[1:])
+    if encrypt_message:
+        return rotate(message, characters_to_rotate)
+    else:
+        return rotate(message, -characters_to_rotate)
+
+
+def generate_call(message,operator,encrypt_message):
+    """
+      this method is responsible for spliting the operation based on their
+      formats and call the appropriate transformation function
+    :param message:
+    :param operator:
+    :param encrypt_message:
+    :return:
+    """
 
     if ',' in operator:
+        # format of exponents are ",exponent"
         operator, exponent = operator.split(',')
+        exponent = int(exponent)
     else:
         exponent = 1
+    if not encrypt_message:
+        # if message is to be decrypted then we reverse the exponents
+        exponent = -exponent
     if operator[0] == "S":
-        return shift(message,int(operator[1:]),int(exponent))
+        return shift(message,int(operator[1:]),exponent)
     elif operator[0] == "R":
-        if len(operator) == 1:
-            return rotate(message)
-        return rotate(message, int(operator[1:]))
+        return make_call_rotate(message,operator,encrypt_message)
     elif operator[0] == "D":
-        return duplicate(message,int(operator[1:]),int(exponent))
+        if not encrypt_message:
+            return "Messages encrypted with duplcate tranformation cannot be " \
+                   "decrypted"
+        else:
+            return duplicate(message,int(operator[1:]),exponent)
     elif operator[0] == "T":
+        # need to handle decryption
         if "(" in operator:
             group_size, left = operator.split(')')
-            group_size = group_size[group_size.index('(')+1:]
-            return group_swap(message,int(left),int(exponent),int(group_size) )
+            group_size = group_size[group_size.index('(') + 1:]
+            if encrypt_message:
+                result = group_swap(message,int(left),exponent,int(group_size))
+                return result
+            else:
+                # decrypt
+                result = group_swap(message,int(left),-exponent,
+                                     int(group_size))
+                return result.replace('$','')
         else:
-            return swap(message, int(operator[1:]), int(exponent))
+            if encrypt_message:
+                return swap(message, int(operator[1:]), exponent)
+            else:
+                return swap(message, int(operator[1:]), -exponent)
 
 
 def process_message(message, operations,encrypt_message):
-    operations = operations.strip().split(';')
+    """
+    This is an intermediary method to stack up the result of multiple
+    transformation
+    :param message:
+    :param operations:
+    :param encrypt_message:
+    :return:
+    """
+    # multiple transformations are seperated by semi colon
+    operations = operations.strip().split(OPERATIONS_SEPERATOR)
+    # remove new line characters from the end of the message
     message = message.strip('\n')
+    # if the message is to be decrypted then the transformation needs to be
+    # applied in reverse order to get the original message back
+    if not encrypt_message:
+        operations.reverse()
     for operation in operations:
         if len(operation) > 0:
-             message = generate_call(message,operation)
+            # generate call to the actual transformation function
+             message = generate_call(message,operation,encrypt_message)
     return message
 
 
-
 def prompt_user_for_input():
-
+    """
+    this method prompts user to get the parameters required to carry out
+    transformation of messages
+    :return:
+    """
     message_file = input("please enter the file name of mesages\n")
     instruction_file = input("please enter the file name of intructions\n")
     output_file = input("please provide a name for the generated output file\n")
@@ -195,38 +240,102 @@ def prompt_user_for_input():
     return message_file,instruction_file,output_file,encrypt_message
 
 
-def get_file_names():
-    if sys.argv == 5:
+def get_required_params_and_execute():
+    """
+    this method is responsible for getting the main parameters required for
+    the program and trigger the encryption or decryption mechanism
+    :return:
+    """
+    if len(sys.argv) == 5:
         # means user provided all the required params in the command line
         read_files_and_process(sys.argv[1],sys.argv[2],sys.argv[3],sys.argv[
-            4]=='e')
+            4] == 'e')
     else:
+        # user did not provide all the required parameters thus will be
+        # asking user for each input
         message_file, instruction_file, output_file, encrypt_message = prompt_user_for_input()
+        # once the parameters are received from the user, initiate the file
+        # processing
         read_files_and_process(message_file,instruction_file,output_file,
                                encrypt_message=='e')
 
 
 def process_output(output,output_file):
+    """
+    this method simply deals with how to show user the out put.
+    writes output to terminal and to the provided output file
+    :param output:
+    :param output_file:
+    :return:
+    """
     with open(output_file,'a') as out:
-        out.write(output+ '\n')
+        out.write(output + '\n')
     print(output)
 
 
 def create_output_file(output_file):
+    """
+    this method creates the output file
+    :param output_file:
+    :return:
+    """
     with open(output_file,'w') as f:
         f.close()
 
 
+def check_file_exists(list_of_input_files):
+    """
+    this method receives the list of files which the program is suppose to
+    read and process,
+    this method ensures that the mentioned files are present if not then
+    terminate the program
+    :param list_of_input_files:
+    :return:
+    """
+    for file in list_of_input_files:
+        try:
+            with open(file,'r') as input_file:
+                input_file.close()
+        except FileNotFoundError:
+            print(file + " does not exist")
+            exit(1)
+
 
 def read_files_and_process(message_file,operations_file,output_file,
                            encrypt_message):
+    """
+    this method is the main driver method responsible for transformation of
+    messages, execution is done line by line, one line from messages and
+    instruction files are read and processed
+
+    :param message_file:
+    :param operations_file:
+    :param output_file:
+    :param encrypt_message: is the flag used to determine whether the
+    instructions are to be applied for encryption or decryption
+    :return:
+    """
+    files = [message_file,operations_file]
+    # check if all the files that are to be read or present or not
+    check_file_exists(files)
+    # create a output file specified by the user
     create_output_file(output_file)
-    # need top open 3 files message file and operations files
+    # open both the message and instruction files, and iterate over each line
+    #  and process message based on the instructions provided
     with open(message_file,'r') as message_lines,open(operations_file,'r') as operaions_lines :
+        # to make iteration over both file possible, combined both the lines
+        # into a tuple, this is only for the purpose of iteration
         for message,operations in zip(message_lines,operaions_lines):
+            # process the message based on the operations given
             output = process_message(message,operations,encrypt_message)
+            # process the output
             process_output(output,output_file)
 
 
-#read_files_and_process('messages.txt','operations.txt','output.txt',True)
-get_file_names()
+
+
+if __name__ == '__main__':
+    """
+    main driver method
+    """
+    get_required_params_and_execute()
