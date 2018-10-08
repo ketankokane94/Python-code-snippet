6b5ed78cc5139f8cf3c17603ce8fbd70b26315a2
ketankokane94 <ketankokane94@gmail.com>
2018-10-08 19:02:54 -0400

    completed the HW 5
    added datetime module

--- Lab5/instrumentation_sort.py
+++ Lab5/instrumentation_sort.py
@@ -1,39 +1,51 @@
+import random
+from datetime import datetime
+
+
+
 MAXIMUM_INT = 9999
 
+def generate_arrays(n):
+    generated_list = list(range(n))
+    random.shuffle(generated_list)
+    return generated_list
 
-def msort(unsorted_list):
+def msort(unsorted_list,comparisons = 0):
     if len(unsorted_list) == 1:
         return unsorted_list,0
     else:
         left_index = 0
         right_index = len(unsorted_list)
         middle_index = (left_index + right_index + 1) // 2
-        return merge(msort(unsorted_list[:middle_index]), msort(unsorted_list[
-                                                         middle_index:]))
+        return merge(msort(unsorted_list[:middle_index],comparisons),msort(unsorted_list[
+                                                               middle_index:]), comparisons)
 
 
-def merge(left_list, right_list):
-    comparisons = 0
+def merge(left_list, right_list,comparisons):
+    left_list, left_compares = left_list[0],left_list[1]
+    right_list, right_compares = right_list[0], right_list[1]
     left_index = right_index = 0
     merged_list = []
+    comparisons = comparisons + left_compares + right_compares
     while left_index < len(left_list) and right_index < len(right_list):
+        comparisons += 1
         if left_list[left_index] <= right_list[right_index]:
-            comparisons +=1
             merged_list.append(left_list[left_index])
             left_index += 1
         else:
             merged_list.append(right_list[right_index])
             right_index += 1
-            comparisons += 1
+
 
     # reaching here means either of the list got exhausted
+    comparisons += 1
     if left_index == len(left_list):
         # left list was exhausted
         merged_list.extend(right_list[right_index:])
     else:
         # right list was exhausted
         merged_list.extend(left_list[left_index:])
-    return merged_list,  comparisons
+    return merged_list, comparisons
 
 
 def find_min(unsorted_list,from_index=1,comparisons=0):
@@ -57,9 +69,21 @@ def ssort(unsorted_list):
     return  unsorted_list,comparisons
 
 if __name__ == '__main__':
-    elements = [3,4,8,2]
-    print("unsorted List:",end='  ')
-    print(elements)
-    print("sorted List:", end='  ')
-    #print(msort(unsorted_list=elements))
-    print(ssort(unsorted_list=elements))
+    print('\tALGORITHM'.ljust(20), 'N'.ljust(6), 'COMPARISONS'.ljust(10),
+          'MICROSECS'.ljust(10))
+    for N in [1,10,100,1000,10000]:
+
+        print('\tSELECTION SORT'.ljust(20), end='\t')
+        print(str(N).ljust(6),end='\t')
+        start = datetime.now()
+        print(str(ssort(unsorted_list=generate_arrays(N))[1]).ljust(10),
+              end='\t')
+        end = datetime.now()
+        print((end - start).microseconds)
+        print('\tMERGE SORT'.ljust(20),end='\t')
+        print(str(N).ljust(6), end='\t')
+        start = datetime.now()
+        print(str(msort(unsorted_list=generate_arrays(N))[1]).ljust(10),
+              end='\t')
+        end = datetime.now()
+        print((end - start).microseconds)
