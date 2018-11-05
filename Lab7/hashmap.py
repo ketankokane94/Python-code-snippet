81200e6b49837c837ab7ee4ec6fca19d77b8f49a
ketankokane94 <ketankokane94@gmail.com>
2018-11-05 15:18:46 -0500

    aded the method to read a dictionary and compare the performance

--- Lab7/hashmap.py
+++ Lab7/hashmap.py
@@ -1,5 +1,6 @@
-__author__ = 'zjb'
+__author__ = 'kk'
 from collections import namedtuple
+import re
 
 Entry = namedtuple('Entry', ('key', 'value'))
 
@@ -8,8 +9,26 @@ To make sure that the DELETED sentinel does not match
 anything we actually want to have in the table, make it
 a unique (content-free!) object.
 '''
-class _delobj: pass
+
+class _delobj:
+    pass
+
+
 DELETED = Entry(_delobj(),None)
+
+
+def hash_func(key):
+    '''
+    Not using Python's built in hash function here since we want to
+    have repeatable testing...
+    However it is terrible.
+    Assumes keys have a len() though...
+    :param key: Key to store
+    :return: Hash value for that key
+    '''
+    # if we want to switch to Python's hash function, uncomment this:
+    # return hash(key)
+    return len(key)
 
 
 def get_sum_of_difference(list_of_ord_values):
@@ -32,13 +51,13 @@ def hash_function1(string):
     return sum
 
 
-
 class Hashmap:
 
     __slots__ = 'table','numkeys', 'capacity', 'maxload','hashfunction', \
                 'probe_count','collision_count'
 
-    def __init__(self, hashfunction, initial_size=100, maxload=0.7):
+    def __init__(self, hashfunction, initial_size=100,
+                 maxload=0.7):
         '''
         Creates an open-addressed hash map of given size and maximum load factor
         :param initial_size: Initial size (default 100)
@@ -59,6 +78,7 @@ class Hashmap:
         :param value: Value of new entry
         '''
         index = self.hashfunction(key) % self.capacity
+        print('index for word',key, 'is ',index)
         start = index
         while self.table[index] is not None and \
                         self.table[index] != DELETED and \
@@ -69,6 +89,9 @@ class Hashmap:
         if self.table[index] is None:
             self.numkeys += 1
         self.table[index] = Entry(key,value)
+        if start != index:
+            self.collision_count += 1
+
         if self.numkeys/self.capacity > self.maxload:
             # rehashing
             oldtable = self.table
@@ -80,8 +103,7 @@ class Hashmap:
             for entry in oldtable:
                 if entry is not None and entry != DELETED:
                     self.put(entry.key,entry.value)
-        if start!= index:
-            self.collision_count += 1
+
         self._increment_probe_count(start,index)
 
 
@@ -135,18 +157,7 @@ class Hashmap:
         self._increment_probe_count(start,index)
         return self.table[index] is not None
 
-    def hash_func(self,key):
-        '''
-        Not using Python's built in hash function here since we want to
-        have repeatable testing...
-        However it is terrible.
-        Assumes keys have a len() though...
-        :param key: Key to store
-        :return: Hash value for that key
-        '''
-        # if we want to switch to Python's hash function, uncomment this:
-        #return hash(key)
-        return len(key)
+
 
     def _increment_probe_count(self, start, index):
         if start > index:
@@ -160,13 +171,16 @@ def printMap(map):
         print(str(i)+": " + str(map.table[i]))
 
 def testMap():
-    map = Hashmap(hash_function1, initial_size=5)
+    #map = Hashmap(hash_func, initial_size=5)
+    map = Hashmap(hash_function1, initial_size=500)
     map.put('apple',1)
     map.put('banana',2)
     map.put('orange',15)
     printMap(map)
     print('probes = ', map.probe_count)
     print('collision = ', map.collision_count)
+
+
     print(map.contains('apple'))
     print(map.contains('grape'))
     print(map.get('orange'))
@@ -188,5 +202,28 @@ def testMap():
 
 
 
+
 if __name__ == '__main__':
-    testMap()
+    words = []
+    with open('/usr/share/dict/words') as file:
+        for lines in file:
+            if lines is not '':
+                word = re.split('\W+',lines.strip())
+                for w in word:
+                    words.append(w)
+    print(len(words))
+
+    map = Hashmap(hash_function1, initial_size=236223)
+    for _ in range(10000):
+        try:
+            word = words[_]
+            value = map.get(word)
+            map.put(word,value + 1)
+        except:
+            map.put(word,1)
+    print('probes = ', map.probe_count)
+    print('collision = ', map.collision_count)
+
+
+
+
