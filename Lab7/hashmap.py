195d7926051edbd477d3c2da03ea4e75f6d77713
ketankokane94 <ketankokane94@gmail.com>
2018-11-05 06:48:46 -0500

    added one hash function

---
+++ Lab7/hashmap.py
@@ -0,0 +1,165 @@
+__author__ = 'zjb'
+from collections import namedtuple
+
+Entry = namedtuple('Entry', ('key', 'value'))
+
+'''
+To make sure that the DELETED sentinel does not match
+anything we actually want to have in the table, make it
+a unique (content-free!) object.
+'''
+class _delobj: pass
+DELETED = Entry(_delobj(),None)
+
+
+def get_sum_of_difference(list_of_ord_values):
+    result = 0
+    for _ in range(len(list_of_ord_values)):
+        if _ is 0:
+            result = list_of_ord_values.pop()
+        else:
+            result -= list_of_ord_values.pop()
+    return result
+
+
+def hash_function1(string):
+    sum = 0
+    list_of_ord_values = []
+    for char in string:
+        sum += ord(char)
+        list_of_ord_values.append(ord(char))
+
+    sum += get_sum_of_difference(list_of_ord_values)
+    return sum
+
+class Hashmap:
+
+    __slots__ = 'table','numkeys', 'capacity', 'maxload'
+
+    def __init__(self, initial_size=100, maxload=0.7):
+        '''
+        Creates an open-addressed hash map of given size and maximum load factor
+        :param initial_size: Initial size (default 100)
+        :param maxload: Max load factor (default 0.7)
+        '''
+
+        self.capacity = initial_size
+        self.table = [None for _ in range(self.capacity)]
+        self.numkeys = 0
+        self.maxload = maxload
+
+    def put(self,key,value):
+        '''
+        Adds the given (key,value) to the map, replacing entry with same key if present.
+        :param key: Key of new entry
+        :param value: Value of new entry
+        '''
+        index = self.hash_func(key) % self.capacity
+
+        while self.table[index] is not None and \
+                        self.table[index] != DELETED and \
+                        self.table[index].key != key:
+            index += 1
+            if index == len(self.table):
+                index = 0
+        if self.table[index] is None:
+            self.numkeys += 1
+        self.table[index] = Entry(key,value)
+        if self.numkeys/self.capacity > self.maxload:
+            # rehashing
+            oldtable = self.table
+            # refresh the table
+            self.capacity *= 2
+            self.table = [None for _ in range(self.capacity)]
+            self.numkeys = 0
+            # put items in new table
+            for entry in oldtable:
+                if entry is not None and entry != DELETED:
+                    self.put(entry.key,entry.value)
+
+    def remove(self,key):
+        '''
+        Remove an item from the table
+        :param key: Key of item to remove
+        :return: Value of given key
+        '''
+        index = self.hash_func(key) % self.capacity
+        while self.table[index] is not None and self.table[index].key != key:
+            index += 1
+            if index == len(self.table):
+                index = 0
+        if self.table[index] is not None:
+            self.table[index] = DELETED
+
+
+    def get(self,key):
+        '''
+        Return the value associated with the given key
+        :param key: Key to look up
+        :return: Value (or KeyError if key not present)
+        '''
+        index = self.hash_func(key) % self.capacity
+        while self.table[index] is not None and self.table[index].key != key:
+            index += 1
+            if index == self.capacity:
+                index = 0
+        if self.table[index] is not None:
+            return self.table[index].value
+        else:
+            raise KeyError('Key ' + str(key) + ' not present')
+
+    def contains(self,key):
+        '''
+        Returns True/False whether key is present in map
+        :param key: Key to look up
+        :return: Whether key is present (boolean)
+        '''
+        index = self.hash_func(key) % self.capacity
+        while self.table[index] is not None and self.table[index].key != key:
+            index += 1
+            if index == self.capacity:
+                index = 0
+        return self.table[index] is not None
+
+    def hash_func(self,key):
+        '''
+        Not using Python's built in hash function here since we want to
+        have repeatable testing...
+        However it is terrible.
+        Assumes keys have a len() though...
+        :param key: Key to store
+        :return: Hash value for that key
+        '''
+        # if we want to switch to Python's hash function, uncomment this:
+        #return hash(key)
+        return len(key)
+
+def printMap(map):
+    for i in range(map.capacity):
+        print(str(i)+": " + str(map.table[i]))
+
+def testMap():
+    map = Hashmap(initial_size=5)
+    map.put('apple',1)
+    map.put('banana',2)
+    map.put('orange',15)
+    printMap(map)
+    print(map.contains('apple'))
+    print(map.contains('grape'))
+    print(map.get('orange'))
+
+    print('--------- adding one more to force table resize ')
+    map.put('grape',7)
+    printMap(map)
+
+    print('--------- testing remove')
+    map.remove('apple')
+    printMap(map)
+
+    print('--------- testing add to a DELETED location')
+    map.put('peach',16)
+    printMap(map)
+    print(map.get('grape'))
+
+if __name__ == '__main__':
+    print(hash_function1('cat'))
