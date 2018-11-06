9abbffef403d1169d4b35065202ca03039c7f22c
ketankokane94 <ketankokane94@gmail.com>
2018-11-06 10:49:36 -0500

    added comments and did the performance benchmarking on a novel also

--- Lab7/hashmap.py
+++ Lab7/hashmap.py
@@ -1,4 +1,12 @@
 __author__ = 'kk'
+'''
+this script uses different hash functions to see how they affect the 
+performance of the hash map. also measure the effect of load factor on the 
+hash map.
+
+for benchmarking, the script uses hashmap to store one dictionary with 50000 
+words in it. and does operations on it using different hash functions.
+'''
 from collections import namedtuple
 import re
 
@@ -18,26 +26,55 @@ DELETED = Entry(_delobj(),None)
 
 
 def hash_function2(string):
+    '''
+    this hash function uses bit operation to generate a number based on the
+    string passed.
+
+    algo used :
+    iterate over the string, for every character in the string
+    find its ordinal value, left shift it by 3
+    then to keep it bounded and make the posistion of the char in the string
+    imp take it mod with one of the 5 prime numbers
+    on every character take the mod of the calculated hash to keep it bounded
+    :param string:
+    :return:
+    '''
     prime_numbers = [179, 127, 233, 283, 103]
     hash = 1
     _ = 0
     for char in string:
+        # take ordinal value and bit shift by 3
         int_value = ord(char) << 3
+        # mod it with one of the 5 prime number to keep it bounded
         int_value = int_value % prime_numbers[ord(char) % 5]
         if _ % 2 is 0:
+            # to keep the total hash value bounded mod it with a bigger prime
+            #  number
             hash = hash % 102233
         _ += 1
+        # keep building the hash value
         hash *= int_value
     return hash
 
 
 def hash_function1(string):
+    '''
+
+    :param string:
+    :return:
+    '''
+    # list of random prime numbers used to keep the hash bounded
     prime_numbers = [179,127,233,283,103]
+    # we use summation to build up the final hash, so iniialise it to 0
     sum = 0
-    _ = 0
+
     for char in string:
+        # for every character in the string calculate the ordinal value
         ordinal_value = ord(char)
+        # multiply the ordinal value with one of the prime number this is
+        # basically to pump up the values
         ordinal_value *= prime_numbers[ordinal_value % 5]
+        # build up the final hash
         sum += ordinal_value
     return sum
 
@@ -83,8 +120,12 @@ class Hashmap:
             self.numkeys += 1
         self.table[index] = Entry(key,value)
 
+        # if the index (hash) given by the hash function has been chaneged,
+        # then it implies that a collision occurred.
+
         if start != index:
             self.collision_count += 1
+
 
         self._increment_probe_count(start, index)
 
@@ -155,10 +196,22 @@ class Hashmap:
 
 
     def _increment_probe_count(self, start, index):
+        '''
+        start is the hash code retured by the hash function,
+        based on the size of the hash map calculate the how far the index
+        moved from the original generated hash value, the distance cane be
+        thought of as the probe.
+        :param start:
+        :param index:
+        :return:
+        '''
+
         if start > index:
             # means wrapping up happened
             self.probe_count += self.capacity - start
+            # reset it to start
             start = 0
+        # how far off is the index from start, that many probes where made
         self.probe_count += index - start + 1
 
 
@@ -166,41 +219,18 @@ def printMap(map):
     for i in range(map.capacity):
         print(str(i)+": " + str(map.table[i]))
 
-def testMap():
-    #map = Hashmap(hash_func, initial_size=5)
-    map = Hashmap(hash_function1, initial_size=500)
-    map.put('apple',1)
-    map.put('banana',2)
-    map.put('orange',15)
-    printMap(map)
-    print('probes = ', map.probe_count)
-    print('collision = ', map.collision_count)
-
-
-    print(map.contains('apple'))
-    print(map.contains('grape'))
-    print(map.get('orange'))
-
-
-
-    print('--------- adding one more to force table resize ')
-    map.put('grape',7)
-    printMap(map)
-
-    print('--------- testing remove')
-    map.remove('apple')
-    printMap(map)
-
-    print('--------- testing add to a DELETED location')
-    map.put('peach',16)
-    printMap(map)
-    print(map.get('grape'))
-
 
 def read_words_from_dict():
+    '''
+    creates a word list from either the dictionary or the alice in wonderland book
+    :return:
+    '''
     words = []
+    #with open('alice.txt') as file:
     with open('/usr/share/dict/words') as file:
         for lines in file:
+            # when the number of words increases the performance is really
+            # slowed down, so keeping the max limit to 50000
             if lines is not '' and len(words) <= 50000:
                 words.extend(re.split('\W+',lines.strip().lower()))
     return words
@@ -210,7 +240,16 @@ def test_map_with_hash_function_and_load_factor(hash_functin_name,
                                                 hash_function,
                                                 load_factor,
                                                 words):
-    map = Hashmap(hash_function, initial_size=30000,maxload=load_factor)
+    '''
+    helper function which runs the hash map experiment with different hash
+    functions and different load factor
+    :param hash_functin_name:
+    :param hash_function:
+    :param load_factor:
+    :param words:
+    :return:
+    '''
+    map = Hashmap(hash_function, initial_size=5000,maxload=load_factor)
     for _ in range(len(words)):
         try:
             word = words[_]
@@ -250,9 +289,6 @@ def main():
 if __name__ == '__main__':
     main()
 
-
-
-# TODO, make a new hash function, send the file thru the novels and
 # TODO  : dictionary and tabulate the result and upload the code
 
 
