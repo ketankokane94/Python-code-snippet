06cdfbab743b410094a1647c38d01e1dc80652cd
ketankokane94 <ketankokane94@gmail.com>
2018-12-06 10:32:28 -0500

    completed the code

--- Lab9/escape.py
+++ Lab9/escape.py
@@ -3,6 +3,11 @@ from graph import Graph
 
 
 def set_maze_dimensions(line):
+    '''
+    this function sets the required dimensions of the maze
+    :param line:
+    :return:
+    '''
     line = line.split(' ')
     global NO_OF_ROWS
     global NO_OF_COLS
@@ -12,14 +17,30 @@ def set_maze_dimensions(line):
     ESCAPE_ROW = int(line[2])
 
 
-def create_vertex(graph, row_number, line,):
+def create_vertex(graph, row_number, line):
+    '''
+    function which takes a line read from file and decides wheather to create a vertex of it or no
+    :param graph:
+    :param row_number:
+    :param line:
+    :return:
+    '''
     for index in range(len(line)):
         if line[index] is '.':
             graph.addVertex(str(index) + ',' + str(row_number) )
 
 
 def read_maze_from_file(graph, filename = 'test.txt'):
+    '''
+    this function is responsible for reading the text file which contains the maze and calls appropriate function to
+    start creating the graph required to solve the maze
+    :param graph:
+    :param filename:
+    :return:
+    '''
+    # variable to track if the first line is read or not
     first_line_read = False
+    # row number to keep track of the naming of the vertex
     row_number = 0
     with open(filename) as maze:
         for line in maze:
@@ -41,12 +62,26 @@ def rows(row,v,maze):
 
 
 def add_neighbours_on_same_row_of_given_vertex(vertex, maze):
+    '''
+    add neighbours to the given vertex on the same row
+    :param vertex:
+    :param maze:
+    :return:
+    '''
     rows(vertex.id.split(',')[1], vertex, maze)
 
 
 def anything_to_left(vertex, maze):
+    '''
+    function which if there is anything to the left side of the given vertex
+    it could either have a wall or rock or nothing
+    appropriate actions are taken based on it
+    :param vertex:
+    :param maze:
+    :return:
+    '''
     if vertex is None:
-        return None
+        return 'node'
     row_num = int(vertex.id.split(',')[1])
     col_num = int(vertex.id.split(',')[0])
     col_num -= 1
@@ -57,15 +92,23 @@ def anything_to_left(vertex, maze):
             return 'rock'
         else:
             # has some node
-            return None
+            return 'node'
     else:
         # has wall
         return 'wall'
 
 
 def anything_to_right(vertex, maze):
+    '''
+    function which if there is anything to the right side of the given vertex
+    it could either have a wall or rock or nothing
+    appropriate actions are taken based on it
+    :param vertex:
+    :param maze:
+    :return:
+    '''
     if vertex is None:
-        return None
+        return 'node'
     row_num = int(vertex.id.split(',')[1])
     col_num = int(vertex.id.split(',')[0])
     col_num += 1
@@ -76,15 +119,23 @@ def anything_to_right(vertex, maze):
             return 'rock'
         else:
             # has some node
-            return None
+            return 'node'
     else:
         # has wall
         return 'wall'
 
 
 def anything_to_top(vertex, maze):
+    '''
+    function which if there is anything to the left side of the given vertex
+    it could either have a wall or rock or nothing
+    appropriate actions are taken based on it
+    :param vertex:
+    :param maze:
+    :return:
+    '''
     if vertex is None:
-        return None
+        return 'node'
     row_num = int(vertex.id.split(',')[1])
     col_num = int(vertex.id.split(',')[0])
     row_num -= 1
@@ -95,15 +146,23 @@ def anything_to_top(vertex, maze):
             return 'rock'
         else:
             # has some node
-            return None
+            return 'node'
     else:
         # has wall
         return 'wall'
 
 
 def anything_below(vertex, maze):
+    '''
+    function which if there is anything to the below side of the given vertex
+    it could either have a wall or rock or nothing
+    appropriate actions are taken based on it
+    :param vertex:
+    :param maze:
+    :return:
+    '''
     if vertex is None:
-        return None
+        return 'node'
     row_num = int(vertex.id.split(',')[1])
     col_num = int(vertex.id.split(',')[0])
     row_num += 1
@@ -114,7 +173,7 @@ def anything_below(vertex, maze):
             return 'rock'
         else:
             # has some node
-            return None
+            return 'node'
     else:
         # has wall
         return 'wall'
@@ -122,6 +181,7 @@ def anything_below(vertex, maze):
 
 def check_if_vertex_has_anything_on_left(vertex, maze,output_list):
     to_left = anything_to_left(vertex, maze)
+
     if to_left is 'rock' :
         # add nodes which are to the right side of the current node
         for _ in range(int(vertex.id.split(',')[0])+1,NO_OF_COLS):
@@ -136,7 +196,7 @@ def check_if_vertex_has_anything_on_left(vertex, maze,output_list):
 def check_if_vertex_has_anything_on_right(vertex, maze,output_list):
     to_right = anything_to_right(vertex, maze)
 
-    if to_right is 'rock'or to_right is 'wall':
+    if to_right is 'rock' or to_right is 'wall':
         # add nodes which are to the right side of the current node
         for _ in range(int(vertex.id.split(',')[0]) - 1, -1, -1):
             node = maze.getVertex(str(_) + ',' + vertex.id.split(',')[1])
@@ -145,8 +205,6 @@ def check_if_vertex_has_anything_on_right(vertex, maze,output_list):
             elif node.id not in output_list:
                 vertex.addNeighbor(node, 1)
                 output_list.append(node.id)
-    elif to_right is 'wall':
-        pass
 
 
 def check_if_vertex_has_anything_on_top(vertex, maze,output_list):
@@ -162,8 +220,6 @@ def check_if_vertex_has_anything_on_top(vertex, maze,output_list):
             elif node.id not in output_list:
                 vertex.addNeighbor(node, 1)
                 output_list.append(node.id)
-    elif above is 'wall':
-        pass
 
 
 def check_if_vertex_has_anything_on_bottom(vertex, maze,output_list):
@@ -181,8 +237,6 @@ def check_if_vertex_has_anything_on_bottom(vertex, maze,output_list):
             elif node.id not in output_list:
                 vertex.addNeighbor(node, 1)
                 output_list.append(node.id)
-    elif below is 'wall':
-        pass
 
 
 def add_connections(maze, vertex,output_list):
@@ -196,6 +250,7 @@ def add_connections(maze, vertex,output_list):
             add_connections(maze, node, output_list)
 
 
+
 def add_vertexes_on_escape_row(maze, escape):
     # maze escape is always on the last column
     col = NO_OF_COLS - 1
@@ -204,6 +259,8 @@ def add_vertexes_on_escape_row(maze, escape):
         node = maze.getVertex(str(_)+ ','+ str(row))
         if node is not None:
             escape.addNeighbor(node,1)
+        else:
+            break
 
 
 def __findPathDFS(current, end, visited):
@@ -247,6 +304,26 @@ def findPathDFS(start, end):
     return __findPathDFS(start, end, visited)
 
 
+def get_path_every_bode_to_escape(maze, escape):
+    paths = {'cannot escape': []}
+
+    for node in maze.getVertices():
+        if node is not escape:
+            path = findPathDFS(escape, maze.getVertex(node))
+            if path is not None:
+                if len(path) - 1 in paths:
+                    lst = paths[len(path) - 1]
+                    lst.append(node)
+                    paths[len(path) - 1] = lst
+                else:
+                    paths[len(path) - 1] = [node]
+            else:
+                lst = paths['cannot escape']
+                lst.append(node)
+                paths['cannot escape'] = lst
+    return paths
+
+
 def test():
     maze = Graph()
 
@@ -260,7 +337,6 @@ def test():
     escape = maze.getVertex('escape')
     # connect vertices on escape node
     add_vertexes_on_escape_row(maze,escape)
-    #add_neighbours_on_same_row_of_given_vertex(escape, maze)
 
     output_list = []
 
@@ -268,42 +344,27 @@ def test():
         output_list.append(ele.id)
 
     print(output_list)
-    '''
-    #check_if_vertex_has_anything_on_bottom(maze.getVertex('1,3'),maze,
-                                           output_list)
-                                           '''
+
     for node in escape.getConnections():
         add_connections(maze,node,output_list)
+
     print(output_list)
+
     print(len(output_list))
-    paths = {}
 
-
-    for node in maze.getVertices():
-        if node is not escape:
-            path = findPathDFS(escape,maze.getVertex(node))
-            if path is not None:
-                if len(path)-1 in paths:
-                    lst = paths[len(path)-1]
-                    lst.append(node)
-                    paths[len(path)-1] = lst
-                else:
-                    paths[len(path)-1] = [node]
-
+    paths = get_path_every_bode_to_escape(maze,escape)
 
     for key in paths.keys():
-        print(key ,paths[key])
+        print(key, paths[key])
 
-    #print(paths)
+    print('---------- output ---------')
+    for index in range(1,len(paths.keys())):
 
+        if index in paths.keys() and paths[index] is not None or []:
+            print(index,paths[index])
 
-
-
-
-
-
-
-
+    if paths['cannot escape'] is not None:
+        print('cannot escape',paths['cannot escape'])
 
 
 if __name__ == '__main__':
